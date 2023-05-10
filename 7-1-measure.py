36bb566ab98dbbe2811d13daf7ca573b7a3d6c8b
BoredLudleth <111277087+BoredLudleth@users.noreply.github.com>
2023-05-10 13:48:38 +0300

    Add files via upload

--- 7-1-measure.py
+++ 7-1-measure.py
@@ -1,100 +1,110 @@
 import RPi.GPIO as GPIO
-from time import sleep
+
+import time
+
 import matplotlib.pyplot as plt
-import numpy as np
 
-GPIO.setmode (GPIO.BCM)
-
-leds = [21,20, 16, 12, 7, 8, 25, 24]
 
 dac = [26, 19, 13, 6, 5, 11, 9, 10]
+
+led = [21, 20, 16, 12, 7, 8, 25, 24]
 
 comp = 4
 
 troyka = 17
 
+GPIO.setmode(GPIO.BCM)
+
 GPIO.setup(dac, GPIO.OUT)
-GPIO.setup(leds, GPIO.OUT)
-GPIO.setup(troyka,GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(comp, GPIO.OUT)
 
-def getTroykaU ():
-    return GPIO.input(troyka)
+GPIO.setup(troyka, GPIO.OUT, initial = GPIO.LOW)
 
-array = [0, 0, 0, 0, 0, 0, 0, 0]
+GPIO.setup(led, GPIO.OUT)
 
-def decimal2binary (value):
-    return [int (element) for element in bin (value)[2:].zfill(8)]
+GPIO.setup(comp, GPIO.IN)
 
-def binary2list(array, value):
-    for i in range (0, 8):
-        if (value & pow(2, i) == pow(2, i)):
-            array[7 - i] = 1
-        else:
-            array[7 - i] = 0
-    return array
+#GPIO.output(dac, 0)
+#GPIO.output(led, 0)
+#GPIO.cleanup()
+
+def decimal_to_binary(value):
+    return [int(element) for element in  bin(value)[2:].zfill(8)]
 
 def adc():
-    k=0
-    for i in range(7, -1, -1):
-        k+=2**i
-        GPIO.output(dac, decimal2binary(k))
-        sleep(0.005)
-        if GPIO.input(comp)==0:
-            k-=2**i
-    return k
+    count = 0
+    for value in range(7, -1, -1):
+        count += 2**value
+        signal = decimal_to_binary(count)
+        GPIO.output(dac, signal)
+        time.sleep(0.01)
+        compval = GPIO.input(comp)
+        if compval == 0:
+            count -= 2**value
+    return count 
 
-f = open("data.txt", 'w')
 
-f2 = open("setting.txt", 'w')
-
+def leds_on(value):
+    c = 0
+    for i in range(8):
+        if (i/8)*256 <= value:
+            c += 2**i
+    GPIO.output(led, decimal_to_binary(c))
+  
 try:
-    step = 0.05
+    res = []
+    time_begin = time.time()
+    point = 0
+    count = 0
 
-    data = []
-    i = 0
-    current_data = 0
+    #зарядка
+    GPIO.output(troyka, 0)
+    time.sleep(5)
+    GPIO.output(troyka, 1)
+
+    while point < 40:
+        point = adc()
+        print(point)
+        res.append(point*3.3/256)
+        print("{:.3f}".format(point*3.3/256))
+        count += 1
+        leds_on(point)
+
+    #разрядка
+    GPIO.output(troyka, 0)
+
+    while point > 10:
+        point = adc()
+        res.append(point*3.3/256)
+        print("{:.3f} V".format(point*3.3/256))
+        count += 1
+        leds_on(point)
     
-    GPIO.output (troyka, 0)
-    sleep (3)
-    GPIO.output (troyka, 1)
-
-    while (current_data < 0.65):
-        current_data = adc() * 3.3 / 255
-        data.append(current_data)
-        print(current_data)
-        sleep(step)
-        i += 1
-
-    GPIO.output (troyka, 0)
-    GPIO.output (dac, [0,0,0,0,0,0,0,0])
-
-    while (current_data > 0.2):
-        current_data = adc() * 3.3 / 255
-        data.append(current_data)
-        print(current_data)
-        sleep(step)
-        i += 1
-
-    for j in data:
-        f.write(str(j)+'\n')
-
-
-    f2.write('Шаг '+str(step)+'\n')
-    f2.write('Частота дикретизации '+str(3.3/256)+'\n')
-    x = np.arange(0, 101, 1)
-    plt.plot(data)
+    time_end = time.time()
+    
+    total_time =  time_end - time_begin
+    print("{:.3f} c".format(total_time))
+    plt.title("График зависимости U от t") # заголовок
+    plt.xlabel("t") # ось абсцисс
+    plt.ylabel("U") # ось ординат
+    plt.grid()      # включение отображение сетки
+    #time_ar = [i for i in range(int(total_time)) ]
+    #plt.plot(res, "r--")  # построение графика
+    plt.plot(res)
     plt.show()
 
-    print ("step = ", step)
-    print ("sampling frequency = ", 1/step)
-    
-
-
+    with open('data.txt', 'w') as f:
+        for i in res:
+            f.write(str(i) + '\n')
+    with open('settings.txt', 'w') as f:
+        f.write(str(1/total_time/count))
+        f.write('\n')
+        f.write(str(3.3/256))
 
 except KeyboardInterrupt:
-        print ("KeyboardInterrupt")
+    print('KeyboardInterrupt')
+
 finally:
-    GPIO.output (dac, [0,0,0,0,0,0,0,0])
-    GPIO.cleanup()
-    command = 0
+    GPIO.output(dac, 0)
+    GPIO.output(led, 0)
+    GPIO.output(troyka, 0)
+    GPIO.cleanup()    
